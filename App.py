fe221e5ea575f476efd9bfb1f63a27b5a262319d
ethan-s-obrien <eobrienit@outlook.com>
2024-12-28 20:13:24 +0900

    Beginnings of general layout and index designs, html and styling

--- App.py
+++ App.py
@@ -7,3 +7,15 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
