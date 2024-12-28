76e93552926fce022e4691820a4744b2587040eb
ethan-s-obrien <eobrienit@outlook.com>
2024-12-28 10:15:42 +0900

    Beginnings of web  app layout

---
+++ App.py
@@ -0,0 +1,9 @@
+import os
+import sqlite3
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
