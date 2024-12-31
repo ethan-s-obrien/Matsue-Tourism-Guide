d0371f5e4563d07fed82a7192a4712aa549a58db
ethan-s-obrien <eobrienit@outlook.com>
2024-12-31 19:25:56 +0900

    Sqlite database setup to handle answers to survey and logic in app.py to call database. Adjustments to questions and styling for survey.

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, g, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -12,6 +12,24 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+app.config['DATABASE'] = os.path.join(app.instance_path, 'database.db')
+os.makedirs(app.instance_path, exist_ok=True)
+
+
+def get_db():
+    """Connect to the database."""
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    """Close the database connection."""
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE Users (
+user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+username TEXT NOT NULL, 
+email TEXT NOT NULL,
+hash TEXT NOT NULL
+);
+
+CREATE TABLE SurveyResponses ( 
+response_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+user_id INTEGER NOT NULL, 
+submitted_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Answers (
+answer_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+response_id INTEGER,
+question TEXT,
+answer TEXT,
+FOREIGN KEY (response_id) REFERENCES SurveyResponses(response_id)
+);
+
+CREATE TABLE TripLength (
+time_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+response_id INTEGER,
+days INTEGER,
+hours INTEGER,
+FOREIGN KEY (response_id) REFERENCES SurveyResponses(response_id)
+);
+
+CREATE TABLE AgeBrackets (
+age_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+response_id INTEGER,
+age_bracket TEXT,
+num_people INTEGER,
+FOREIGN KEY (response_id) REFERENCES SurveyResponses(response_id)
+);
+
+CREATE TABLE Preferences (
+preference_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+response_id INTEGER,
+rank INTEGER,
+preference TEXT,
+FOREIGN KEY (response_id) REFERENCES SurveyResponses(response_id)
+);
